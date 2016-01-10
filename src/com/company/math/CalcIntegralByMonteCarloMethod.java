7c1c7fa89520469f77e3c8f4de402c60303980ea
Yevhen Khomiak <khomyak_eugene@yahoo.com>
2016-01-10 14:56:40 +0200

    implementing of additional task of Unit 4

--- src/com/company/math/CalcIntegralByMonteCarloMethod.java
+++ src/com/company/math/CalcIntegralByMonteCarloMethod.java
@@ -1,22 +1,81 @@
 package com.company.math;
 
+import com.company.utils.SelfDescribingObjectService;
+import java.lang.reflect.Method;
+
 /**
  * Created by Yevgen on 09.01.2016.
  */
-public abstract class CalcIntegralByMonteCarloMethod {
-    abstract public double function(double argument);
 
-    public double integral(double startPoint, double finishPoint, int precision) {
+public class CalcIntegralByMonteCarloMethod {
+    private String functionClassName;
+    private String functionMethodName;
+    private Method function = null;
+
+    public CalcIntegralByMonteCarloMethod(String functionClassName, String functionMethodName) {
+        this.functionClassName = functionClassName;
+        this.functionMethodName = functionMethodName;
+
+        if (!functionClassName.isEmpty() && !functionMethodName.isEmpty()) {
+            initFunction();
+        }
+    }
+
+    public CalcIntegralByMonteCarloMethod(String functionClassName) {
+        this(functionClassName, "");
+    }
+
+    private void initFunction() {
+        this.function = SelfDescribingObjectService.searchOneDoubleArgumentPublicMethod(functionClassName, functionMethodName);
+    }
+
+    public String getFunctionClassName() {
+        return functionClassName;
+    }
+
+    public void setFunctionClassName(String functionClassName) {
+        this.functionClassName = functionClassName;
+
+        initFunction();
+    }
+
+    public String getFunctionMethodName() {
+        return functionMethodName;
+    }
+
+    public void setFunctionMethodName(String functionMethodName) {
+        this.functionMethodName = functionMethodName;
+
+        initFunction();
+    }
+
+    public Method getFunction() {
+        if (function == null) {
+            initFunction();
+        }
+
+        return function;
+    }
+
+    public double calcIntegral(double startPoint, double finishPoint, int precision) {
         double result = 0.0;
+        Method func = getFunction();
+
+        // Correct input parameter mistake if necessary
+        if (finishPoint < startPoint) {
+            double temp = startPoint;
+            startPoint = finishPoint;
+            finishPoint = temp;
+        }
 
         double currentPoint = startPoint;
         double length = (finishPoint - startPoint);
-        double delta = length / (precision - 1);
+        double delta = length / precision;
         for (int i = 0; i < precision; i++) {
-            result += function(currentPoint);
+            result += SelfDescribingObjectService.invokeOneDoubleArgumentStaticMethod(func, currentPoint);
             currentPoint += delta;
         }
 
-        return (result * (length / precision));
+        return result * delta;
     }
 }
--- src/com/company/tests/TestMonteCarloIntegralCalculation.java
+++ src/com/company/tests/TestMonteCarloIntegralCalculation.java
@@ -1,18 +1,47 @@
 package com.company.tests;
 
-import com.company.utils.SelfDescribingObjectService;
+import com.company.math.CalcIntegralByMonteCarloMethod;
 import com.company.utils.Utils;
+
+import java.text.MessageFormat;
 
 /**
  * Created by Yevgen on 09.01.2016.
  */
 public class TestMonteCarloIntegralCalculation {
+    public static final String MATH_CLASS_NAME = "java.lang.Math";
+    public final static String PLEASE_ENTER_A_DOUBLE_METHOD_DOUBLE_NAME_PATTERN =
+            "Please, enter name of a <double> method(<double>) of class {0} (or just type <Return> to stop):";
+    public final static String INPUT_START_POINT_MESSAGE = "Input start point double value:";
+    public final static String INPUT_FINISH_POINT_MESSAGE = "Input start point double value:";
+    public final static String INPUT_PRECISION_MESSAGE = "Input precision int value:";
+    public final static String INTEGRAL_VALUE_PATTERN = "Integral value for function {0}.{1} is {2}";
 
-    public void demonstrateCarloIntegralCalculation () { //я так поняла - тут не дописано?) - 10.01.2016 01:25, Хом'як: угу, це поки що - заготовка
+    public void demonstrateCarloIntegralCalculation () {
+        Utils.writeMessage(Utils.getFullMethodName());
 
-        // Temporary test
-        Utils.writeMessage(
-                Double.toString(SelfDescribingObjectService.invokeOneDoubleArgumentStaticMethod("java.lang.Math","sqrt", 4.0)));
+        // Integral calculator
+        CalcIntegralByMonteCarloMethod calcIntegralByMonteCarloMethod = new CalcIntegralByMonteCarloMethod(MATH_CLASS_NAME);
 
+        do {
+            String methodName = Utils.readInputString(MessageFormat.format(PLEASE_ENTER_A_DOUBLE_METHOD_DOUBLE_NAME_PATTERN, MATH_CLASS_NAME));
+            if (methodName.isEmpty()) break;
+
+            // Store method name
+            calcIntegralByMonteCarloMethod.setFunctionMethodName(methodName);
+            // Is it correct?
+            if (calcIntegralByMonteCarloMethod.getFunction() != null) {
+                // Get input parameters
+                double startPoint = Utils.readInputDouble(INPUT_START_POINT_MESSAGE);
+                double finishPoint = Utils.readInputDouble(INPUT_FINISH_POINT_MESSAGE);
+                int precision = Utils.readInputInt(INPUT_PRECISION_MESSAGE);
+
+                // Calc and show calcIntegral value
+                Utils.writeMessage(MessageFormat.format(INTEGRAL_VALUE_PATTERN,
+                        calcIntegralByMonteCarloMethod.getFunctionClassName(),
+                        calcIntegralByMonteCarloMethod.getFunctionMethodName(),
+                        calcIntegralByMonteCarloMethod.calcIntegral(startPoint, finishPoint, precision)));
+            }
+       } while (true);
     }
 }
