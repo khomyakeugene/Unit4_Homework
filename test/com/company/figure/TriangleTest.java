e77c972e8b654f3db8a158c30949b585c6352391
Yevhen Khomiak <khomyak_eugene@yahoo.com>
2016-01-31 23:45:29 +0200

    Unit 11 Homework

---
+++ test/com/company/figure/TriangleTest.java
@@ -0,0 +1,24 @@
+package com.company.figure;
+
+import org.junit.Test;
+
+/**
+ * Created by Yevgen on 31.01.2016 as a part of the project "Unit4_Homework".
+ */
+public class TriangleTest {
+
+    @Test
+    public void testCalcPerimeter() throws Exception {
+
+    }
+
+    @Test
+    public void testDisplayFigureParameters() throws Exception {
+
+    }
+
+    @Test
+    public void testCalcArea() throws Exception {
+
+    }
+}
