f84f7adb074922e3a5479176b71a4dc4211241a9
Yevhen Khomiak <khomyak_eugene@yahoo.com>
2016-01-31 23:53:53 +0200

    Unit 11 Homework

---
+++ test/com/company/math/CalcIntegralByMonteCarloMethodTest.java
@@ -0,0 +1,14 @@
+package com.company.math;
+
+import org.junit.Test;
+
+/**
+ * Created by Yevgen on 31.01.2016 as a part of the project "Unit4_Homework".
+ */
+public class CalcIntegralByMonteCarloMethodTest {
+
+    @Test
+    public void testCalcIntegral() throws Exception {
+
+    }
+}
---
+++ test/com/company/temperature/TemperatureTest.java
@@ -0,0 +1,19 @@
+package com.company.temperature;
+
+import org.junit.Test;
+
+/**
+ * Created by Yevgen on 31.01.2016 as a part of the project "Unit4_Homework".
+ */
+public class TemperatureTest {
+
+    @Test
+    public void testTransformCelsiusToFahrenheit() throws Exception {
+
+    }
+
+    @Test
+    public void testTransformFahrenheitToCelsius() throws Exception {
+
+    }
+}
